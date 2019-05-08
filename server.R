865dfa2d5677a3fdbbe3b96a044224e09a83cf59
slamrichmond <slamrichmondwebmaster@gmail.com>
2019-05-07 21:57:17 -0400

    color pallete functionality

--- server.R
+++ server.R
@@ -1,30 +1,50 @@
 require(shiny)
 require(jpeg)
+require(dplyr)
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  data.photo <- reactive({
-    readJPEG(input$photo$datapath)
-  })
-  
-  data.photo.pixels <- reactive({
-    data.frame(
-      R = as.vector(data.photo()[,,1]),
-      G = as.vector(data.photo()[,,2]),
-      B = as.vector(data.photo()[,,3])
+  cluster.points.color <- reactive({
+    data.photo <- readJPEG(input$photo$datapath)
+    
+    data.photo.pixels <- data.frame(
+      R = as.vector(data.photo[,,1]),
+      G = as.vector(data.photo[,,2]),
+      B = as.vector(data.photo[,,3])
     )
+    
+    as.data.frame(kmeans(data.photo.pixels, input$color.number)$centers) %>%
+      mutate(R = round(R * 255), G = round(G * 255), B = round(B * 255))
   })
   
-  output$red.average <- renderText({
-    mean(data.photo.pixels()$R)
-  })
-  
-  output$green.average <- renderText({
-    mean(data.photo.pixels()$G)
-  })
-  
-  output$blue.average <- renderText({
-    mean(data.photo.pixels()$B)
+  lapply(1:20, function(row.number){
+    
+    output[[paste0("RGB.", row.number)]] <- renderText({
+      if(row.number <= input$color.number) {
+        paste0(
+          "RGB: ", 
+          paste(
+            cluster.points.color()$R[row.number],
+            cluster.points.color()$G[row.number],
+            cluster.points.color()$B[row.number],
+            sep = ", "
+          )
+        )
+      }
+      else {
+        ""
+      }
+    })
+    
+    output[[paste0("color.panel.", row.number)]] <- renderUI({
+      R <- cluster.points.color()$R[row.number]
+      G <- cluster.points.color()$G[row.number]
+      B <- cluster.points.color()$B[row.number]
+      
+      RGB <- paste(R, G, B, sep = ",")
+      tags$div(style = paste0("height: 30px; weight: 100%; background-color:rgb(", RGB, ")"))
+    })
+    
   })
 }
--- ui.R
+++ ui.R
@@ -2,10 +2,13 @@ require(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-  
+  tags$style(
+    type = "text/css",
+    ".shiny-output-error { visibility: hidden; }",
+    ".shiny-output-error:before { visibility: hidden; }"
+  ),
   # Application title
   titlePanel("Color Palette From Photo"),
-  
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
@@ -15,14 +18,19 @@ ui <- fluidPage(
         "Upload a Photo (must be JPG)",
         multiple = FALSE,
         accept = c(".jpg")
-      )
+      ),
+      numericInput("color.number", "Number of Colors", 5, max = 20)
     ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      textOutput("red.average"),
-      textOutput("green.average"),
-      textOutput("blue.average")
+      uiOutput("color.panel"),
+      lapply(1:20, function(index){
+        list(
+          uiOutput(paste0("color.panel.", index)),
+          textOutput(paste0("RGB.", index))
+        )
+      })
     )
   )
 )
